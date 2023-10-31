e6128b306fe5a2693cb6e6d2ec65007389d6d9b8
RSH <fbtnghks7906@gmail.com>
2023-10-31 22:00:06 +0900

    React UseState 실습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+const heavyWork = () => {
+  console.log('엄청 무거운 작업!!!');
+  return ['홍길동', '김민수'];
+}
 
 function App() {
+  const [names, setNames] = useState(() => {
+    return heavyWork();
+  });
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleUpload = () => {
+    setNames((prevState) => {
+      console.log("이전 state: ", prevState);
+      return [input, ...prevState];
+    });
+  };
+
+  console.log(input);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={input} onChange={handleInputChange} />
+      <button onClick={handleUpload}>Upload</button>
+      {names.map((name, idx) => {
+        return <p key={idx}>{names}</p>;
+      })}
     </div>
   );
 }
